2dcb2e5434ebefdaa17fde436af8a65f6837809b
Patrick Frank <patr.frank@gmx.de>
2022-01-22 16:01:39 +0100

    WIP first functional refactoring

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,3 +1,4 @@
+import chess
 import yaml
 import json
 from pathlib import Path
@@ -6,75 +7,86 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-from chesspos.preprocessing import SampleGenerator, bitboard_to_board
-from chesspos.models import DenseAutoencoder, CnnAutoencoder
+from chesspos.preprocessing import SampleGenerator, tensor_to_board, board_to_tensor
+from chesspos.models import ResnetAutoencoder
 
-evaluate_data_params = yaml.safe_load(open(Path(__file__).with_name('params.yaml')))['evaluate']['data']
-evaluate_eval_params = yaml.safe_load(open(Path(__file__).with_name('params.yaml')))['evaluate']['eval']
+evaluate_data_params = yaml.safe_load(open(Path(__file__).with_name('params_tensor.yaml')))['evaluate']['data']
+evaluate_eval_params = yaml.safe_load(open(Path(__file__).with_name('params_tensor.yaml')))['evaluate']['eval']
+model_params = yaml.safe_load(open(Path(__file__).with_name('params_tensor.yaml')))['train']['model']
+
+sample_preprocessor = lambda samples: tuple([samples.reshape((*samples.shape, 1)), samples.reshape((*samples.shape, 1))])
 
 test_generator = SampleGenerator(
-    sample_dir = evaluate_data_params['test_dir'],
-    batch_size = evaluate_data_params['test_batch_size']
+	sample_dir = evaluate_data_params['test_dir'],
+	sample_preprocessor = sample_preprocessor,
+	batch_size = evaluate_data_params['test_batch_size'],
+	sample_type = np.float32,
 )
-test_generator.set_subsampling_functions(['singlets'])
-test_generator.construct_generator()
 
-autoencoder = CnnAutoencoder(
-    input_size = None,
-    embedding_size = None,
-    hidden_layers = None,
-    loss = None,
-    train_generator = None,
-    test_generator = test_generator,
-	save_dir = evaluate_data_params['model_dir'],
-    train_steps_per_epoch = None,
-    test_steps_per_epoch = None,
-    tf_callbacks = None
+autoencoder = ResnetAutoencoder(
+    train_generator=test_generator,
+	test_generator=test_generator,
+	train_steps_per_epoch=model_params['train_steps_per_epoch'],
+	test_steps_per_epoch=model_params['test_steps_per_epoch'],
+	save_dir=model_params['save_dir'],
+	output_to_board = tensor_to_board,
+	board_to_input = board_to_tensor,
+	loss = model_params['loss'],
+	tf_callbacks = model_params['tf_callbacks'] + [
+		keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.03, patience=5, verbose=1, mode='min', restore_best_weights=True),
+		#keras.callbacks.TensorBoard(log_dir=model_params['save_dir'], histogram_freq=1, write_images=True, embeddings_freq=1)
+	]
 )
 
 autoencoder.load()
 examples = evaluate_eval_params['number_examples']
 examples_out = ""
 
-best_samples = autoencoder.get_best_samples(examples, 10*examples)
-for i in range(examples):
-    best_sample_bitboard = best_samples[i]['position'].reshape((773, -1))
-    best_sample_position = bitboard_to_board(best_sample_bitboard)
-    examples_out += str(best_samples[i]['loss']) + '\n\n'
-    examples_out += autoencoder.compare_sample_to_prediction(best_sample_bitboard)
+start_board = chess.Board()
+prediction = autoencoder.predict_from_board([start_board])
+prediction = prediction[0,:,:,:,0]
+prediction = autoencoder.binarize_array(prediction)
+print(prediction[:,:,0])
+print(prediction[:,:,1])
+print(tensor_to_board(prediction).__str__())
 
-worst_samples = autoencoder.get_worst_samples(examples, 10)
-for i in range(examples):
-    worst_sample_bitboard = worst_samples[i]['position'].reshape((773, -1))
-    worst_sample_position = bitboard_to_board(worst_sample_bitboard)
-    examples_out += str(worst_samples[i]['loss']) + '\n'
-    examples_out += autoencoder.compare_sample_to_prediction(worst_sample_bitboard)
+# best_samples = autoencoder.get_best_samples(examples)
+# for i in range(examples):
+#     best_sample_input = best_samples[i]['input']
+#     examples_out += str(best_samples[i]['loss']) + '\n\n'
+#     examples_out += autoencoder.compare_input_to_prediction(best_sample_input)
 
-print(examples_out)
+# worst_samples = autoencoder.get_worst_samples(examples)
+# for i in range(examples):
+#     worst_sample_input = worst_samples[i]['input']
+#     examples_out += str(worst_samples[i]['loss']) + '\n'
+#     examples_out += autoencoder.compare_input_to_prediction(worst_sample_input)
 
-with open(evaluate_eval_params['result_dir']+'/examples.out', 'w') as file:
-    file.write(examples_out)
+#print(examples_out)
+
+# with open(evaluate_eval_params['result_dir']+'/examples.out', 'w') as file:
+#     file.write(examples_out)
 
 
-with open(evaluate_eval_params['result_dir']+'/scores.json', 'w') as file:
-    json.dump(
-		{
-			'train_loss': autoencoder.train_history['loss'][-1],
-			'test_loss': autoencoder.train_history['val_loss'][-1]
-		},
-		file,
-		indent=2
-	)
+# with open(evaluate_eval_params['result_dir']+'/scores.json', 'w') as file:
+#     json.dump(
+# 		{
+# 			'train_loss': autoencoder.train_history['loss'][-1],
+# 			'test_loss': autoencoder.train_history['val_loss'][-1]
+# 		},
+# 		file,
+# 		indent=2
+# 	)
 
-with open(evaluate_eval_params['result_dir']+'/test_scores.json', 'w') as file:
-    json.dump(
-		{
-			'val_loss': [{
-				'epoch': i,
-				'train_loss': autoencoder.train_history['loss'][i],
-				'val_loss':  autoencoder.train_history['val_loss'][i]
-			} for i in range(len(autoencoder.train_history['val_loss']))]
-		},
-		file,
-		indent=2
-	)
+# with open(evaluate_eval_params['result_dir']+'/test_scores.json', 'w') as file:
+#     json.dump(
+# 		{
+# 			'val_loss': [{
+# 				'epoch': i,
+# 				'train_loss': autoencoder.train_history['loss'][i],
+# 				'val_loss':  autoencoder.train_history['val_loss'][i]
+# 			} for i in range(len(autoencoder.train_history['val_loss']))]
+# 		},
+# 		file,
+# 		indent=2
+# 	)
--- train_model.py
+++ train_model.py
@@ -1,3 +1,4 @@
+import chess
 import yaml
 from pathlib import Path
 import numpy as np
@@ -5,47 +6,64 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
-from chesspos.preprocessing import SampleGenerator
-from chesspos.models import CnnResnetAutoencoder
+from chesspos.preprocessing import SampleGenerator, board_to_tensor, tensor_to_board
+from chesspos.models import ResnetAutoencoder
 
 data_params = yaml.safe_load(open(Path(__file__).with_name('params_tensor.yaml')))['train']['data']
 model_params = yaml.safe_load(open(Path(__file__).with_name('params_tensor.yaml')))['train']['model']
 
+start_board = chess.Board()
+start_tensor = board_to_tensor(start_board)
+print(start_tensor)
+slice0 = start_tensor[:,:,0]
 
 sample_preprocessor = lambda samples: tuple([samples.reshape((*samples.shape, 1)), samples.reshape((*samples.shape, 1))])
-autoencoder_postprocessor = lambda tensor: tensor.reshape(tensor.shape[:-1])
 
 train_generator = SampleGenerator(
 	sample_dir = data_params['train_dir'],
 	sample_preprocessor = sample_preprocessor,
 	batch_size = data_params['train_batch_size'],
-	sample_type = bool
+	sample_type = np.float32,
 )
 
 test_generator = SampleGenerator(
 	sample_dir = data_params['test_dir'],
 	sample_preprocessor = sample_preprocessor,
 	batch_size = data_params['test_batch_size'],
-	sample_type = bool
+	sample_type = np.float32,
 )
 
-autoencoder = CnnResnetAutoencoder(
-	input_size = model_params['input_size'],
-	embedding_size = model_params['embedding_size'],
-	hidden_layers = model_params['hidden_layers'],
+generator_test = test_generator.get_generator()
+sample = next(generator_test)
+#print(f"len(sample): {len(sample)}")
+#print(f"sample[0].shape: {sample[0].shape}")
+#print(f"sample[0].dtype: {sample[0].dtype}")
+#board = tensor_to_board(sample[0][0,:,:,:,0])
+#print(board.__str__())
+#print(f"sample[0][0,:,:,:,0]: {sample[0][0,:,:,:,0]}")
+
+autoencoder = ResnetAutoencoder(
+	train_generator=train_generator,
+	test_generator=test_generator,
+	train_steps_per_epoch=model_params['train_steps_per_epoch'],
+	test_steps_per_epoch=model_params['test_steps_per_epoch'],
+	save_dir=model_params['save_dir'],
+	output_to_board = tensor_to_board,
+	board_to_input = board_to_tensor,
 	loss = model_params['loss'],
-	train_generator = train_generator,
-	test_generator = test_generator,
-	save_dir = model_params['save_dir'],
-	train_steps_per_epoch = model_params['train_steps_per_epoch'],
-	test_steps_per_epoch = model_params['test_steps_per_epoch'],
 	tf_callbacks = model_params['tf_callbacks'] + [
 		keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.03, patience=5, verbose=1, mode='min', restore_best_weights=True),
-		keras.callbacks.TensorBoard(log_dir=model_params['save_dir'], histogram_freq=1, write_images=True, embeddings_freq=1)
+		#keras.callbacks.TensorBoard(log_dir=model_params['save_dir'], histogram_freq=1, write_images=True, embeddings_freq=1)
 	]
 )
 
-autoencoder.build_model()
-autoencoder.compile()
 history = autoencoder.train()
 autoencoder.save()
+
+start_board = chess.Board()
+prediction = autoencoder.predict_from_board([start_board])
+prediction = prediction[0,:,:,:,0]
+prediction = autoencoder.binarize_array(prediction)
+print(prediction[:,:,0])
+print(prediction[:,:,1])
+print(tensor_to_board(prediction).__str__())
