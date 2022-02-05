1251c124568a30f054f9551d99d89007f98c81cd
patrickfrank <25801668+patrickfrank1@users.noreply.github.com>
2022-02-05 18:22:34 +0100

    Refactor autoencoder model (#8)
    reflects API changes made in chess-position parent repo

--- extract_positions.py
+++ extract_positions.py
@@ -1,11 +1,12 @@
+from typing import Callable
 import h5py
 import yaml
 import logging
 import argparse
 from pathlib import Path
-from chesspos.preprocessing import (
-	PgnExtractor, get_game_processor, get_game_filter
-)
+import chess
+
+from chesspos.preprocessing import *
 
 if __name__ == "__main__":
 
@@ -16,11 +17,21 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	params = yaml.safe_load(open(Path(__file__).with_name(args.config)))['preprocess']
 
+	game_filter = None
+	if params['game_filter'] == 'custom':
+		def custom_filter(header: chess.pgn.Headers):
+			return filter_by_elo(header, white_elo_range=[2600,4000], black_elo_range=[2600,4000]) and \
+				filter_by_time_control(header, time_range=[5,60])
+		game_filter = custom_filter
+	else:
+		game_filter = get_game_filter(params['game_filter'])
+
+
 	position_extractor = PgnExtractor(
 		pgn_path=params['pgn_path'],
 		save_path=params['save_path'],
 		game_processor=get_game_processor(params['game_processor']),
-		game_filter=get_game_filter(params['game_filter']),
+		game_filter=game_filter,
 		chunk_size=params['chunk_size'],
 		log_level=logging.INFO
 	)
